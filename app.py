6345e42490f9791b5a08a791ad80d70dd00f84af
kimt1975 <kim.traulsen@gmail.com>
2025-03-04 21:34:58 +0100

    Fixed host and port for Render deployment

--- app.py
+++ app.py
@@ -22,5 +22,8 @@ def get_sponsorships():
         mimetype='application/json'
     )
 
+import os
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Brug Render's port, ellers 5000
+    app.run(host="0.0.0.0", port=port, debug=True)  # Lyt på alle netværk
